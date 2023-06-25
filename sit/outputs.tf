2852fb2398e53dc6f5b9c5c4b62830fc22ffa004
Simi-DevOps <simiosiefa@gmail.com>
2023-06-25 18:45:25 +0100

    update

--- sit/outputs.tf
+++ sit/outputs.tf
@@ -7,10 +7,10 @@ output "aws_vpc" {
   value = aws_vpc.Task5_vpc.id
 }
 
-output "subnet" {
+output "subnet_pub1" {
   value = aws_subnet.pub1.id
 }
 
-output "subnet" {
+output "subnet_pub2" {
   value = aws_subnet.pub2.id
 }
