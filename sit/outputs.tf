192d013414324af5d4be70a2e1e4929c7bc13779
Simi-DevOps <simiosiefa@gmail.com>
2023-06-25 12:05:25 +0100

    Task 5 update

--- sit/outputs.tf
+++ sit/outputs.tf
@@ -2,3 +2,15 @@ output "ec2_public_ip" {
   value = aws_instance.ec2_AmazonLinux.public_ip
 
 }
+
+output "aws_vpc" {
+  value = aws_vpc.Task5_vpc.id
+}
+
+output "subnet" {
+  value = aws_subnet.pub1.id
+}
+
+output "subnet" {
+  value = aws_subnet.pub2.id
+}
--- sit/sit.securitygroups.tf
+++ sit/sit.securitygroups.tf
@@ -1,34 +1,26 @@
-
-resource "aws_security_group" "ec2_sg" {
-  name        = "SIT EC2 Security group"
-  description = "allow HTTP and SSH inb traffic"
-  vpc_id      = "vpc-0bb955ba49115e69b"
-
-  ingress {
-    description = "SSH from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.Task5_vpc.id
+}
 
   ingress {
-    description = "HTTP from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
-    Name = "SIT EC2 Security group"
+    Name = "Task 5 sec_grp"
   }
-}
+
